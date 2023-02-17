2e9bba34430c64d225d39a47498298f54212bb3f
maksymprudnik <maksymprudnik@gmail.com>
2023-02-17 10:18:46 +0200

    general - moved to yarn, added json-schema-to-ts, updated lib/schema to support conversion to types

--- src/lib/schema.js
+++ src/lib/schema.js
@@ -1,76 +1,18 @@
-/** @typedef {import('./schema')} Schema */
-/** @typedef {import('./schema').SchemaItem} SchemaItem */
-
-/** @type Schema['paginate'] */
-export const paginate = (item) => ({
-  required: ['count', 'items'],
-  properties: {
-    count: { type: 'integer' },
-    items: {
-      type: 'array',
-      items: {
-        type: 'object',
-        additionalProperties: false,
-        required: item.required ?? [],
-        properties: item.properties,
-      },
-    },
-  },
-});
-
-/** @type Schema['toObject'] */
-export const toObject = (item) => ({
+export const strictObjectProperties = /** @type {const} */ ({
   type: 'object',
   additionalProperties: false,
-  required: item.required,
-  properties: item.properties,
-  nullable: Boolean(item.nullable),
 });
 
-/** @type Schema['toArray'] */
-export const toArray = (item) => ({
-  type: 'array',
-  items: {
-    type: 'object',
-    additionalProperties: false,
-    required: item.required,
-    properties: item.properties,
-  },
-});
-
-/** @type Schema['selectProperties'] */
-export const selectProperties = (entity, properties) => {
-  /** @type {Required<SchemaItem>} */
-  const result = { required: [], properties: {} };
-  for (const property of properties) {
-    if (entity.required?.includes(property)) result.required.push(property);
-    if (property in entity.properties)
-      result.properties[property] = entity.properties[property];
-  }
-  return result;
-};
-
-/** @type Schema['dropProperties'] */
-export const dropProperties = (entity, properties) => {
-  const result = JSON.parse(JSON.stringify(entity)); // update to structuredClone on node18
-  for (const property of properties) {
-    const idx = result.required.indexOf(property);
-    if (idx >= 0) result.required.splice(idx, 1);
-    if (property in result.properties) delete result.properties[property];
-  }
-  return result;
-};
-
-export const paginationProperties = {
+export const paginationProperties = /** @type {const} */ ({
   page: { type: 'integer', minimum: 1, default: 1 },
   limit: { type: 'integer', minimum: 1, default: 10 },
-};
+});
 
-export const sortProperties = {
+export const sortProperties = /** @type {const} */ ({
   sortBy: { type: 'string', default: 'createdAt' },
   sortOrder: {
     type: 'string',
     enum: ['asc', 'desc'],
     default: 'asc',
   },
-};
+});
